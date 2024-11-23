d121f544555ea19053404de6e6d3607180ca2d92
Hipppie3 <107952593+Hipppie3@users.noreply.github.com>
2024-11-23 11:33:20 -0800

    fetched data from backend

--- src/pages/Players.jsx
+++ src/pages/Players.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import './Players.css';
 
 const Players = () => {
+  const [players, setPlayers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchPlayers = async () => {
+      try {
+        const response = await axios.get('/api/players');
+        setPlayers(response.data.players);
+      } catch(error) {
+        console.error("Error fetching players", error);
+        setError("failed to load players. Please try again later.")
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchPlayers();
+},[]);
+
+  if (loading) {
+    return <div className="loading">Loading players...</div>;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
+  if (players.length === 0) {
+    return <div>No players found</div>;
+  }
+
   return (
-    <div>
-      Players
+    <div className='players-container'>
+      {players.map(player => (
+        <div className='player-card' key={player.id}>
+          <img src={player.image} alt={`${player.firstName} ${player.lastName}`} className='player-image' />
+          <h2 className='player-name'>{`${player.firstName} ${player.lastName}`}</h2>
+        </div>
+      ))}
     </div>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      // Proxy all API requests
+      '/api': {
+        target: 'http://localhost:5005', // Backend server
+        changeOrigin: true,
+        secure: false, // For HTTPS (if needed)
+        rewrite: (path) => path.replace(/^\/api/, ''), // Optional: Strip '/api' prefix if not used in backend
+      },
+    },
+  },
+});
